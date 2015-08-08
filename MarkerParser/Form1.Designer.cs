5f2463e2dfb2d47b3affdccab54ce0e7807b3800
iamsurya <s@iamsurya.com>
2015-08-08 09:46:07 -0400

    Updated to Remove Date bug

--- MarkerParser/Form1.Designer.cs
+++ MarkerParser/Form1.Designer.cs
@@ -33,20 +33,27 @@
             this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TBInputFile = new System.Windows.Forms.TextBox();
+            this.TBOutputFile = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TBMarkerFile = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(39, 66);
+            this.button1.Location = new System.Drawing.Point(376, 59);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(108, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Select Input File";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(162, 66);
+            this.button2.Location = new System.Drawing.Point(376, 299);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(110, 23);
             this.button2.TabIndex = 1;
@@ -56,17 +63,18 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(39, 126);
+            this.button3.Location = new System.Drawing.Point(376, 139);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(108, 23);
             this.button3.TabIndex = 2;
             this.button3.Text = "Select Output File";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(39, 190);
+            this.label1.Location = new System.Drawing.Point(43, 304);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(46, 13);
             this.label1.TabIndex = 3;
@@ -75,24 +83,79 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(121, 190);
+            this.label2.Location = new System.Drawing.Point(125, 304);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(22, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "     ";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Input File : ";
+            // 
+            // TBInputFile
+            // 
+            this.TBInputFile.Location = new System.Drawing.Point(128, 59);
+            this.TBInputFile.Name = "TBInputFile";
+            this.TBInputFile.Size = new System.Drawing.Size(228, 20);
+            this.TBInputFile.TabIndex = 6;
+            // 
+            // TBOutputFile
+            // 
+            this.TBOutputFile.Location = new System.Drawing.Point(128, 142);
+            this.TBOutputFile.Name = "TBOutputFile";
+            this.TBOutputFile.Size = new System.Drawing.Size(228, 20);
+            this.TBOutputFile.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Output File : ";
+            // 
+            // TBMarkerFile
+            // 
+            this.TBMarkerFile.Location = new System.Drawing.Point(128, 168);
+            this.TBMarkerFile.Name = "TBMarkerFile";
+            this.TBMarkerFile.ReadOnly = true;
+            this.TBMarkerFile.Size = new System.Drawing.Size(228, 20);
+            this.TBMarkerFile.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(55, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Marker File : ";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(507, 401);
+            this.Controls.Add(this.TBMarkerFile);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TBOutputFile);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TBInputFile);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Shimmer Data Parser";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,6 +168,12 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TBInputFile;
+        private System.Windows.Forms.TextBox TBOutputFile;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TBMarkerFile;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- MarkerParser/Form1.cs
+++ MarkerParser/Form1.cs
@@ -15,6 +15,8 @@ namespace MarkerParser
 {
     public partial class Form1 : Form
     {
+        String InputFileName, OutputFileName, MarkerFileName;
+
         public Form1()
         {
             InitializeComponent();
@@ -22,21 +24,21 @@ namespace MarkerParser
 
         private void button2_Click(object sender, EventArgs e)
         {
+
+            bool First = true; // Bool To get the starting time
+            String SomeDate;
+            DateTime SomeDateTime;
+
             // Console.WriteLine("Hello!");
-            label2.Text = @"Started";
+            label2.Text = @"Working";
+            label2.ForeColor = Color.Red;
             label2.Refresh();
             //Application.DoEvents();
-            
 
+            StreamWriter MarkerWriter = new StreamWriter(File.OpenWrite(MarkerFileName));
+            StreamWriter DataWriter = new StreamWriter(File.OpenWrite(OutputFileName));
 
-            String MarkerOutputFName = @"C:\temp\data1-markers.txt";
-            String DataOutputFName = @"C:\temp\data1.txt";
-            // String Fname = @"C:\temp\data1-markers.txt";
-
-            var MarkerWriter = new StreamWriter(File.OpenWrite(MarkerOutputFName));
-            var DataWriter = new StreamWriter(File.OpenWrite(DataOutputFName));
-
-            var reader = new StreamReader(File.OpenRead(@"C:\temp\data1.csv"));
+            var reader = new StreamReader(File.OpenRead(InputFileName));
 
             /* Read first line with seperator information */
             var line = reader.ReadLine();
@@ -48,18 +50,35 @@ namespace MarkerParser
 
             /* Read first line with column units */
             line = reader.ReadLine();
-
-            /* Read actual line */
-            line = reader.ReadLine();
             values = line.Split('\t');
             var time = values[16].Split('_');
-            
+
+
+            /* Read actual lines */
             while (!reader.EndOfStream)
             {
+                line = reader.ReadLine();
 
                 values = line.Split('\t');
 
                 DataWriter.WriteLine(values[0] + "\t" + values[3] + "\t" + values[4] + "\t" + values[5] + "\t" + values[6] + "\t" + values[7] + "\t" + values[8] + "\t" + values[9] + "\t" + values[10] + "\t" + values[11] + "\t" + values[12] + "\t" + values[13] + "\t" + values[14] + "\t" + values[15]);
+                
+                /* Is this the start of the data ? */
+                /* Write the START Marker Details */
+                if(First == true)
+                {
+                    time = values[16].Split('_');
+                    if(time[0].Length < 4)
+                        SomeDate = time[0].Substring(0,1) + " " + time[1].ToString() + " " + time[2].ToString();
+                    else
+                        SomeDate = time[0].Substring(0, 2) + " " + time[1].ToString() + " " + time[2].ToString();
+                    SomeDateTime = Convert.ToDateTime(SomeDate);
+                    MarkerWriter.WriteLine("START\t" + SomeDateTime.Date.ToString("u").Substring(0,10) + "\t" + time[3].ToString().Substring(0, 8));
+
+                    First = false;
+                }
+
+                /* Is this a Marker ? */
                 if ((Double.Parse(values[2])) > 0)
                 {
                     
@@ -68,13 +87,70 @@ namespace MarkerParser
                     MarkerWriter.WriteLine("MARKER\t" + time[3].ToString().Substring(0, 8));
 
                 }
-                line = reader.ReadLine();
+                
 
             }
 
+
+            /* Write the END Marker details */
+            time = values[16].Split('_');
+            if (time[0].Length < 4)
+                SomeDate = time[0].Substring(0, 1) + " " + time[1].ToString() + " " + time[2].ToString();
+            else
+                SomeDate = time[0].Substring(0, 2) + " " + time[1].ToString() + " " + time[2].ToString();
+            SomeDateTime = Convert.ToDateTime(SomeDate);
+            MarkerWriter.WriteLine("END\t" + SomeDateTime.Date.ToString("u").Substring(0, 10) + "\t" + time[3].ToString().Substring(0, 8));
+
+            /* Close the Writer streams */
             MarkerWriter.Close();
             DataWriter.Close();
             label2.Text = @"Finished";
+            label2.ForeColor = Color.Green;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the open file dialog box.
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            // Set filter options and filter index.
+            openFileDialog1.Filter = "CSV Files (.csv)|*.csv|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+
+            // Call the ShowDialog method to show the dialog box.
+            DialogResult userClickedOK = openFileDialog1.ShowDialog();
+
+             // Process input if the user clicked OK.
+            if (userClickedOK == DialogResult.OK)
+            {
+                InputFileName = openFileDialog1.FileName;
+                TBInputFile.Text = InputFileName;
+                OutputFileName = new DirectoryInfo(InputFileName).Parent.Name;
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SaveFileDialog1 = new SaveFileDialog();
+
+            SaveFileDialog1.Filter = "Shimmer Data Files (.txt)|*.txt|All Files (*.*)|*.*";
+            SaveFileDialog1.FilterIndex = 1;
+            SaveFileDialog1.FileName = OutputFileName + ".txt";
+            
+
+            DialogResult userClickedOK = SaveFileDialog1.ShowDialog();
+
+            // Process input if the user clicked OK.
+            if (userClickedOK == DialogResult.OK)
+            {
+                OutputFileName = SaveFileDialog1.FileName;
+                TBOutputFile.Text = OutputFileName;
+                MarkerFileName = OutputFileName.Substring(0, OutputFileName.Length - 4) + "-markers.txt";
+                TBMarkerFile.Text = MarkerFileName;
+
+            }
         }
     }
 }
